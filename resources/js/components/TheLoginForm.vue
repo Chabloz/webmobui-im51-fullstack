a991e2d6dd0f3c93f3216839e12de7ebb0548c53
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2024-04-30 11:02:27 +0200

    Add blade layout for Vue app and manage CSRF in fetchApi composable

---
+++ resources/js/components/TheLoginForm.vue
@@ -0,0 +1,63 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const name = defineModel('name');
+  const password = defineModel('password');
+  const isPwd = ref(true);
+
+  function submit(evt) {
+    evt.target.submit();
+  }
+
+  function reset() {
+    name.value = '';
+    password.value = '';
+    isPwd.value = true;
+  }
+</script>
+
+<template>
+  <q-form
+    action="/login"
+    method="post"
+    @submit="submit"
+    @reset="reset"
+    autocomplete="off"
+  >
+    <q-input
+      filled
+      v-model="name"
+      name="name"
+      label="Username"
+      hint="Your username"
+      lazy-rules="ondemand"
+      :rules="[ val => val && val.match(/^[a-z]+$/i) && val.length > 1 || 'Alphabetic only, at least 2']"
+    />
+
+    <q-input
+      v-model="password"
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      hint="Your password"
+      lazy-rules="ondemand"
+      :rules="[ val => val && val.length > 7 || 'Password required']"
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isPwd = !isPwd"
+        />
+      </template>
+    </q-input>
+
+    <div>
+      <q-btn label="Submit" type="submit" color="primary"/>
+      <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+    </div>
+  </q-form>
+</template>
+
+<style scoped>
+
+</style>
