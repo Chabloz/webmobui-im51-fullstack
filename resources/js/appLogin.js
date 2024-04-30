14389e3bd784cc4d473b19902a068f6ae60581a4
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2024-04-30 09:15:35 +0200

    Add form example

--- resources/js/appLogin.js
+++ resources/js/appLogin.js
@@ -2,11 +2,17 @@ import { createApp } from 'vue';
 import { Quasar } from 'quasar';
 import App from './AppLogin.vue';
 
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
 
 import 'quasar/dist/quasar.css';
 
 const myApp = createApp(App)
-myApp.use(Quasar, {plugins: {}});
+myApp.use(Quasar, {
+  plugins: {},
+  config: {
+    brand: { negative: 'tomato' },
+    dark: 'auto',
+  },
+});
 myApp.mount('#app');
